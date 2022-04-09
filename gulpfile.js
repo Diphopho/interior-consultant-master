cabe305b9e2acd6815eb7727403834d6485d022c
Ertantiyo <ertantiyo@gmail.com>
2022-04-09 18:12:39 +0700

    push gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const babel = require("gulp-babel");
+const terser = require("gulp-terser");
+const browserSync = require("browser-sync");
+
+//Sass Task
+function scssTask() {
+  return src("src/scss/main.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest("dist/css", { sourcemaps: "." }));
+}
+
+//JS Task
+function jsTask() {
+  return src("src/js/main.js", { sourcemaps: true })
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(terser())
+    .pipe(dest("dist/js", { sourcemaps: "." }));
+}
+
+//browserSync Task
+function browserSyncServe(cb) {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: 0,
+      },
+    },
+  });
+  cb();
+}
+function browserSyncReload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+//Watch Task
+function watchTask() {
+  watch("*.html", browserSyncReload);
+  watch(
+    ["src/scss/**/*.scss", "src/js/*.js"],
+    series(scssTask, jsTask, browserSyncReload)
+  );
+}
+
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+// function defaultTask(cb) {
+//   // place code for your default task here
+//   cb();
+// }
+
+// exports.default = defaultTask;
